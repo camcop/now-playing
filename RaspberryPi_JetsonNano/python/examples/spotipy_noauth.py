794a2b4353634ae414d19ad0b7e99fedea4109c7
Cameron Copland <cjcopland@gmail.com>
2024-02-20 16:51:44 +0000

    load variables from .env

--- RaspberryPi_JetsonNano/python/examples/spotipy_noauth.py
+++ RaspberryPi_JetsonNano/python/examples/spotipy_noauth.py
@@ -1,6 +1,12 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
+from dotenv import load_dotenv
+
+try:
+    load_dotenv()
+except ImportError:
+    logging.warning('No dotenv found')
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ["SPOTIPY_CLIENT_ID"],
                                                client_secret=os.environ["SPOTIPY_CLIENT_SECRET"]))
